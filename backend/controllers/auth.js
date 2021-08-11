3fe2fea30b01f723fe1e3790792fcc71c1dadb29
restrictedCodex <restrictedcharudutta@gmail.com>
2021-08-11 22:45:05 +0530

    routes

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,16 @@
 const User = require("../models/user");
-
+const { validationResult} = require('express-validator');
 
 exports.signup = (req,res) => {
+
+    const error = validationResult(req);
+
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            err: error.array()[0].msg
+        })
+    }
+
     const user = new User(req.body);
     user.save((err, user) => {
         if(err){
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var router = express.Router()
+const { check } = require('express-validator');
 
 const {signout, signup}  = require("../controllers/auth")
 
-router.post("/signup", signup)
+router.post("/signup",[
+    check("name").isLength({min: 3}).withMessage('name must be at least 3 chars long'),
+    check("email").isEmail().withMessage('enter valid email'),
+    check("password").isLength({min: 6}).withMessage('password must be at least 3 chars long'),
+], signup)
 router.get("/signout", signout)
 
 module.exports = router;
