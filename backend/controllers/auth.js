1be4e4b7e8d967468c8edf973b03deb1c0ad7292
restrictedCodex <restrictedcharudutta@gmail.com>
2021-08-12 12:42:11 +0530

    custom middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,35 +1,102 @@
 const User = require("../models/user");
-const { validationResult} = require('express-validator');
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
-exports.signup = (req,res) => {
+exports.signup = (req, res) => {
+  const error = validationResult(req);
 
-    const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).json({
+      err: error.array()[0].msg,
+    });
+  }
 
-    if(!error.isEmpty()){
-        return res.status(422).json({
-            err: error.array()[0].msg
-        })
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT WORKING",
+      });
     }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
+  });
+};
 
-    const user = new User(req.body);
-    user.save((err, user) => {
-        if(err){
-            return res.status(400).json({
-                err: "NOT WORKING"
-            })
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).json({
+      err: error.array()[0].msg
+    });
+  }
+
+  User.findOne({email}, (err, user) => {
+        if (err || !user) {
+          return res.status(400).json({
+              error: "user email dosnt exist",
+          });
         }
-        res.json({
-            name: user.name,
-            email: user.email,
-            id: user._id
+
+        if (!user.authenticate(password)) {
+        return res.status(401).json({
+            error: "Email and password dont match",
         });
-    })
+        }
+
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+        res.cookie("token", token, { expire: new Date() + 55});
+
+        const { _id, name, email, role } = user;
+        return res.json({
+        token,
+        user: {
+            _id,
+            name,
+            email,
+            role,
+        },
+        });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "signed out sussec mf",
+  });
+};
+
+
+//protected routes
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth"
+});
+
+//custom middleware
+exports.isAuthenticated = (req, res, next) =>{
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id
+  if(!checker){
+    return res.status(403).json({
+      error: "ACCESS DENIED"
+    });
+  }
+  next();
 }
 
-
-exports.signout = (req,res) => {
-    res.json({
-        message: "signed out mf"
-    })
-} 
-
+exports.isAdmin = (req, res, next) =>{
+  if(req.profile.role === 0){
+    return res.status(403).json({
+      error: "YOU ARE NOT ADMIN"
+    });
+  }
+  next();
+}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,38 @@
-var express = require('express')
-var router = express.Router()
-const { check } = require('express-validator');
+var express = require("express");
+var router = express.Router();
+const { check } = require("express-validator");
 
-const {signout, signup}  = require("../controllers/auth")
+const { signout, signup , signin, isSignedIn} = require("../controllers/auth");
 
-router.post("/signup",[
-    check("name").isLength({min: 3}).withMessage('name must be at least 3 chars long'),
-    check("email").isEmail().withMessage('enter valid email'),
-    check("password").isLength({min: 6}).withMessage('password must be at least 3 chars long'),
-], signup)
-router.get("/signout", signout)
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name must be at least 3 chars long"),
+    check("email").isEmail().withMessage("enter valid email"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("password must be at least 3 chars long"),
+  ],
+  signup
+);
+
+router.post(
+    "/signin",
+    [
+      check("email").isEmail().withMessage("enter valid email"),
+      check("password")
+        .isLength({ min: 6 })
+        .withMessage("password must be at least 3 chars long"),
+    ],
+    signin
+);
+
+router.get("/signout", signout);
+
+router.get("/testroute", isSignedIn, (req,res)=>{
+  res.json(req.auth);
+})
 
 module.exports = router;
