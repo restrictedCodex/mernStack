6264cc1b728730bd2b2228f0967561eddc61488f
restrictedCodex <restrictedcharudutta@gmail.com>
2021-09-04 23:48:32 +0530

    frontend

--- testfront/react/myapp/src/index.js
+++ testfront/react/myapp/src/index.js
@@ -1,28 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Route, Link, Switch, BrowserRouter, Router} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import User from './User';
-import Visit from './Visit';
+import "./index.css";
+import App from "./App";
+import User from "./User";
+import Visit from "./Visit";
 
 function Routing() {
   return (
     <>
       <Router>
-      <Route path="/" exact  component={App} />
-      <Route path="/user"  component={User} />
-      <Route path="/visit"  component={Visit} />
-    </Router>
+        <Route path="/" exact component={App} />
+        <Route path="/user" component={User} />
+        <Route path="/visit" component={Visit} />
+      </Router>
     </>
-  )
+  );
 }
 
-
-
-ReactDOM.render(
-    Routing,
-  document.getElementById('root')
-);
-
+ReactDOM.render(Routing, document.getElementById("root"));
