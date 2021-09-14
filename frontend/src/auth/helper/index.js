c1af8f4806a62c84a388f270f91995e398616e34
Charudutta <restrictedcharudutta@gmail.com>
2021-09-14 22:57:24 +0530

    login done

--- frontend/src/auth/helper/index.js
+++ frontend/src/auth/helper/index.js
@@ -14,3 +14,51 @@ export const signup = (user) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const signin = (user) => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+
+export const signout = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+
+    return fetch(`${API}/signout`, {
+      method: "GET"
+    })
+    .then(res => console.log("signout success"))
+    .catch(err => console.log(err))
+  }
+};
+
+
+export const isAuthenticated = () => {
+  if (typeof window !== "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")){
+    return JSON.parse(localStorage.getItem("jwt"))
+  } else {
+    return false;
+  }
+}
--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -3,38 +3,46 @@ import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
 
-    const signUpForm = () => {
-        return(
-            <div className='row'>
-                <div className='col-md-6 offset-sm-3 text-left'>
-                    <form>
-                        <div className='form-group'>
-                            <label className='text-light'>Name</label>
-                            <input className='form-control' type='text'/>
-                        </div>
-                        <div className='form-group'>
-                            <label className='text-light'>Email</label>
-                            <input className='form-control' type='email'/>
-                        </div>
-                        <div className='form-group'>
-                            <label className='text-light'>Password</label>
-                            <input className='form-control' type='password'/>
-                        </div>
-                        
-                        <button className='btn btn-success btn-block'>Submit</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+  const { name, email, password, error, success } = values;
 
-
+  const signUpForm = () => {
     return (
-        <Base title="Sign Up Page" description="A page for user signup!">
-            {signUpForm()}
-        </Base>
-    )
-}
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input className="form-control" type="text" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+
+            <button className="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign Up Page" description="A page for user signup!">
+      {signUpForm()}
+    </Base>
+  );
+};
 
 export default Signup;
